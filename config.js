e1fe149c6a8dc46d15b0a25d2df0d8ed3377ef40
danny-oliva <methfan@gmail.com>
2026-08-05 14:02:32 -0700

    Add shortName and color properties to calendars

--- config.js
+++ config.js
@@ -5,24 +5,32 @@ export const CONFIG = {
     calendars: {
         varsity: {
             name: "Varsity",
+            shortName: "V",
+            color: "#003B71",
             id: "si0mp7bhmn85leebcrfh1mqtvs@group.calendar.google.com",
             enabled: true
         },
     
         jv: {
             name: "JV",
+            shortName: "JV",
+            color: "#666666",
             id: "mmo9lf13l4kvemp53q5hsfsocc@group.calendar.google.com",
             enabled: true
         },
     
         freshman: {
             name: "Freshman",
+            shortName: "F",
+            color: "#FFFF00",
             id: "ae85c9c65997b1865730a768f565824c72d0341cdaf127ccf99236c683f5da94@group.calendar.google.com",
             enabled: false
         },
     
         froshSoph: {
             name: "Frosh/Soph",
+            shortName: "FS",
+            color: "#0000FF",
             id: "8vufl29g9kca99hc9rifrm9s50@group.calendar.google.com",
             enabled: true
         }
