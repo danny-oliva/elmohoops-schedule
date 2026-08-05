38f9cf63b07ab75f5f465da076afa54b48471878
danny-oliva <methfan@gmail.com>
2026-08-05 13:22:41 -0700

    Add JV and Freshman calendars to config

--- config.js
+++ config.js
@@ -5,7 +5,26 @@ export const CONFIG = {
     calendars: {
         varsity: {
             name: "Varsity",
-            id: "si0mp7bhmn85leebcrfh1mqtvs@group.calendar.google.com"
+            id: "si0mp7bhmn85leebcrfh1mqtvs@group.calendar.google.com",
+            enabled: true
+        },
+    
+        jv: {
+            name: "JV",
+            id: "mmo9lf13l4kvemp53q5hsfsocc@group.calendar.google.com",
+            enabled: true
+        },
+    
+        freshman: {
+            name: "Freshman",
+            id: "ae85c9c65997b1865730a768f565824c72d0341cdaf127ccf99236c683f5da94@group.calendar.google.com",
+            enabled: false
+        },
+    
+        froshSoph: {
+            name: "Frosh/Soph",
+            id: "8vufl29g9kca99hc9rifrm9s50@group.calendar.google.com",
+            enabled: true
         }
     }
 
