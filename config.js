070ee9348c2c0ecf2b0bc81eea3c5813731ce374
danny-oliva <methfan@gmail.com>
2026-08-05 20:16:23 -0700

    Change calendar colors for various teams

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ export const CONFIG = {
         varsity: {
             name: "Varsity",
             shortName: "V",
-            color: "#000000",
+            color: "#003B71",
             id: "si0mp7bhmn85leebcrfh1mqtvs@group.calendar.google.com",
             enabled: true
         },
@@ -18,7 +18,7 @@ export const CONFIG = {
         jv: {
             name: "JV",
             shortName: "JV",
-            color: "#000000",
+            color: "#5F6368",
             id: "mmo9lf13l4kvemp53q5hsfsocc@group.calendar.google.com",
             enabled: true
         },
@@ -26,7 +26,7 @@ export const CONFIG = {
         freshman: {
             name: "Freshman",
             shortName: "F",
-            color: "#000000",
+            color: "#6B4FA3",
             id: "ae85c9c65997b1865730a768f565824c72d0341cdaf127ccf99236c683f5da94@group.calendar.google.com",
             enabled: false
         },
@@ -34,7 +34,7 @@ export const CONFIG = {
         froshSoph: {
             name: "Frosh/Soph",
             shortName: "FS",
-            color: "#000000",
+            color: "#2F6DB3",
             id: "8vufl29g9kca99hc9rifrm9s50@group.calendar.google.com",
             enabled: true
         }
